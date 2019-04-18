422701a3ed35751acb627342695cf45fff887edb
Francisco llamas <francisco.llamas@gmail.com>
2019-04-18 12:50:01 -0500

    sm 02

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-https://alligator.io/react/axios-react/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-class FetchDemo extends React.Component {
+class FetchDatos extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       posts: [],
       loading: true,
@@ -15,23 +13,9 @@ class FetchDemo extends React.Component {
   }
 
   componentDidMount() {
-    console.log("entramos DidMont")
-    // Remove the 'www.' to cause a CORS error (and see the error state)
     axios.get('../api/gPresuntos.php?accion=13-A-09000-14-0741-06-004')
       .then(res => {
 
-        console.dir("hay respuesta..." + JSON.stringify(res.data))
-        // Transform the raw data by extracting the nested posts
-        const posts = res.data
-        console.log("posts: "+posts)
-
-        console.log(res.data);
-        console.log(res.status);
-        console.log(res.statusText);
-        console.log(res.headers);
-        console.log(res.config);
-        // Update state to trigger a re-render.
-        // Clear any errors, and turn off the loading indiciator.
         this.setState({
           posts,
           loading: false,
@@ -39,8 +23,6 @@ class FetchDemo extends React.Component {
         });
       })
       .catch(err => {
-        // Something went wrong. Save the error in state and re-render.
-        console.log("hay error "+err)
         this.setState({
           loading: false,
           error: err
@@ -77,7 +59,6 @@ class FetchDemo extends React.Component {
   render() {
     return (
       <div>
-        <h1>{`/r/${this.props.subreddit}`}</h1>
         {this.state.loading ?
           this.renderLoading()
           : this.renderPosts()}
@@ -86,8 +67,7 @@ class FetchDemo extends React.Component {
   }
 }
 
-// Change the subreddit to anything you like
 ReactDOM.render(
-  <FetchDemo subreddit="Hola papa"/>,
+  <FetchDatos/>,
   document.getElementById('root')
 );
